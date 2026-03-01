4046f43632a9da50c6368e669b92a567eccb325f
Luis Ortega <y456243648@gmail.com>
2026-03-01 16:15:07 -0500

    ejercicios

---
+++ ejercicioos/src/ejercicio1.java
@@ -0,0 +1,17 @@
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        String nombre = "Luis Ortega";
+        int codigo = 33445566;
+        int semestre = 5;
+        float promedio = 3.7f;
+        boolean matriculado = true;
+
+        System.out.println("Nombre del estudiantado: " + nombre);
+        System.out.println("Código: " + codigo);
+        System.out.println("Semestre: " + semestre);
+        System.out.println("Promedio: " + promedio);
+        System.out.println("Matriculado: " + matriculado);
+
+    }
+}
