c3a06d44ae8f6a0a24e038c38c42a32f4b661250
Luis Ortega <y456243648@gmail.com>
2026-03-01 18:12:49 -0500

    create

---
+++ ejercicioos/src/ejercicio4.java
@@ -0,0 +1,17 @@
+public class ejercicio4 {
+    public static void main(String[] args) {
+        String placa = "1234-0109";
+        int añoFabricacion = 2009;
+         int CilCC = 400;
+        double precomp = 45000.50;
+        char incol = 'A';
+        boolean sovin = true;
+        
+        System.out.println("Placa: " + placa);
+        System.out.println("Año de fabricación: " + añoFabricacion);
+        System.out.println("Cilindraje en cc: " + CilCC);
+        System.out.println("Precio de compra: " + precomp);
+        System.out.println("Inicial del color: " + incol);
+        System.out.println("Tiene SOAT vigente: " + sovin);
+    }
+}
